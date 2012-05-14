5ddbec1ebd38be93d77e2fd33c2886bdcb8c19aa
Andrew M <fileformat@gmail.com>
2012-05-13 21:06:44 -0400

    turned on debug flag to help figure out problems with machine translation (which I did not find yet)

--- bin/gtrans.sh
+++ bin/gtrans.sh
@@ -5,6 +5,6 @@
 
 for f in ../text/??.po
 do
-	./gt4po.py --apikey=`cat /etc/fileformatnet/google-translate.key` --destlang=`basename $f .po` ../text/en.po
+	./gt4po.py --debug --apikey=`cat /etc/fileformatnet/google-translate.key` --destlang=`basename $f .po` ../text/en.po
 done
 
